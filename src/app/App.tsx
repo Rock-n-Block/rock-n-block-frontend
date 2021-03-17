e5122eaf79767d6e16a47cb591741a862610b686
TheJuze <dimanikus0106@gmail.com>
2021-03-17 14:37:05 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        App works
+      </header>
+    </div>
+  );
+}
+
+export default App;
